b075679a5816d7c385db794d0e14ea910bb3556a
stanleyoyz <song407b@gmail.com>
2020-06-19 13:21:25 +0800

    added unity project by stanley

---
+++ 39_PA11_stanley/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private int score = 0;
+    public Text scoretxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        transform.position = new Vector3(0, Mathf.Clamp(transform.position.y, -3.95f, 3.95f), 0);
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("gameover");
+
+        }
+        if(other.gameObject.CompareTag("scoretrigger"))
+        {
+            score += 1;
+            scoretxt.text = "score : " + score;
+        }
+    }
+}
---
+++ 39_PA11_stanley/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] myObjects;
+
+    
+
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    void SpawnObjects()
+    {
+        int randomIndex = Random.Range(0, myObjects.Length);
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(myObjects[randomIndex], transform.position, transform.rotation);
+    }
+
+}
